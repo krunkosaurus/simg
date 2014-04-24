9c097dede6fe1b7408b8bdd9498c7d1d6f246f77
Mauvis Ledford <mauvis@brainswap.me>
2014-04-24 12:28:22 -0700

    Broke apart bigger methods into smaller ones that reference each other and Added .noConflict().

--- simg.js
+++ simg.js
@@ -1,11 +1,35 @@
-(function(global){
-  Simg = global.Simg = function(svg){
+(function(root){
+  var previousSimg = root.Simg;
+
+  Simg = root.Simg = function(svg){
     this.svg = svg;
   }
+
+  Simg.noConflict(){
+    root.Simg = previousSimg;
+    return this;
+  }
+
+  Simg.getBase64Image = function(img) {
+    // From: http://stackoverflow.com/questions/934012/get-image-data-in-javascript
+    var canvas = document.createElement("canvas");
+    canvas.width = img.width;
+    canvas.height = img.height;
+
+    var ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0);
+    var dataURL = canvas.toDataURL("image/png");
+
+    return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+  };
 
   Simg.prototype = {
     // Return SVG text.
     toString: function(svg){
+      if (!svg){
+        throw new Error('.toString: No SVG found.');
+      }
+
       [
         ['version', 1.1],
         ['xmlns', "http://www.w3.org/2000/svg"],
@@ -15,43 +39,77 @@
       return svg.parentNode.innerHTML;
     },
 
-    // Return new img from SVG.
-    // Call with no arguments to return svg image element.
-    // Call with callback to return png image element.
-    toImg: function(cb){
+    // Return canvas with this SVG drawn inside.
+    toCanvas: function(cb){
+      this.toSvgImage(function(img){
+        var canvas = document.createElement('canvas');
+        var context = canvas.getContext("2d");
+
+        canvas.width = img.width;
+        canvas.height = img.height;
+
+        context.drawImage(img, 0, 0);
+        cb(canvas);
+      });
+    },
+
+    toSvgImage: function(cb){
       var str = this.toString(this.svg);
       var img = document.createElement('img');
 
-      var canvas = document.createElement('canvas');
-      var context = canvas.getContext("2d");
-
       if (cb){
-        img.onload = function() {
-          canvas.width = img.width;
-          canvas.height = img.height;
-
-          context.drawImage(img, 0, 0);
-
-          var canvasData = canvas.toDataURL("image/png");
-          var pngImg = document.createElement('img');
-
-          pngImg.onload = function(){
-            cb(pngImg);
-          }
-          pngImg.setAttribute('src', canvasData);
+        img.onload = function(){
+          cb(img);
         };
       }
 
+      // Make the new img's source an SVG image.
       img.setAttribute('src', 'data:image/svg+xml;base64,'+ btoa(str));
-      return img;
+    },
+
+    // Returns callback to new img from SVG.
+    // Call with no arguments to return svg image element.
+    // Call with callback to return png image element.
+    toImg: function(cb){
+      this.toCanvas(function(canvas){
+        var canvasData = canvas.toDataURL("image/png");
+        var img = document.createElement('img');
+
+        img.onload = function(){
+          cb(img);
+        }
+
+        // Make pngImg's source the canvas data.
+        img.setAttribute('src', canvasData);
+      });
     },
 
     // Replace SVG with PNG img.
-    replace: function(){
+    replace: function(cb){
       var self = this;
       this.toImg(function(img){
         var parentNode = self.svg.parentNode;
         parentNode.replaceChild(img, self.svg);
+        if (cb){
+          cb();
+        }
+      });
+    },
+
+    // Converts canvas to binary blob.
+    toBinaryBlob: function(cb){
+      this.toCanvas(function(canvas){
+        var dataUrl = canvas.toDataURL().replace(/^data:image\/(png|jpg);base64,/, "");
+        var byteString = atob(dataUrl);
+        // write the bytes of the string to an ArrayBuffer
+        var ab = new ArrayBuffer(byteString.length);
+        var ia = new Uint8Array(ab);
+        for (var i = 0; i < byteString.length; i++) {
+          ia[i] = byteString.charCodeAt(i);
+        }
+        var dataView = new DataView(ab);
+        var blob = new Blob([dataView], {type: "image/png"});
+        cb(blob);
       });
     },
 
